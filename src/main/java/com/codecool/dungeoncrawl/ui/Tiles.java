5483a7e596eeec4993222a2f4ead96e101dc035f
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-07-18 13:29:19 +0200

    door is exist but dont require key

--- src/main/java/com/codecool/dungeoncrawl/ui/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/Tiles.java
@@ -30,6 +30,7 @@ public class Tiles {
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("key", new Tile(16, 23));
         tileMap.put("sword", new Tile(0, 29));
+        tileMap.put("door", new Tile(6, 17));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
