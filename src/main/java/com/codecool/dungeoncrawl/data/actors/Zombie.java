c33e152459a70571e26c3b9bdbb92460f4f853bd
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-07-18 14:35:02 +0200

    fiex helath bug

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Zombie.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Zombie.java
@@ -5,7 +5,9 @@ import com.codecool.dungeoncrawl.data.Cell;
 public class Zombie extends Actor{
     private  int health = 15;
     public Zombie(Cell cell) {
+
         super(cell);
+        this.health = 15;
     }
 
     @Override
